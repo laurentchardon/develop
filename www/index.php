df5847d77487ae742caced6ca8e5633baace6467
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-09-10 13:18:05 +0000

    link to other-copyrights.php

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@
 
 <TABLE WIDTH="98%" CELLPADDING="0" CELLSPACING="0" BORDER="0">
 <TR>
-   <TD><A HREF="/"><IMG SRC="/images/freshports.jpg" ALT="FreshPorts.org - the place for ports" WIDTH="512" HEIGHT="110" BORDER="0"></A>
+   <TD><A HREF="/other-copyrights.php"><IMG SRC="/images/freshports.jpg" ALT="FreshPorts.org - the place for ports" WIDTH="512" HEIGHT="110" BORDER="0"></A>
   </TD>
    <TD ALIGN="right" CLASS="sans" VALIGN="bottom"><small><? echo date("D, j M Y g:i A T") ?></small>
   </TD>
