a3a36571bb5980396fec77c6ea58f9527e3f2cc7
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2002-09-10 13:17:56 +0000

    Add other copyrights

---
+++ www/other-copyrights.php
@@ -0,0 +1,47 @@
+<HTML>
+
+<HEAD><TITLE>FreshPorts Development Site - other copyrights</TITLE></HEAD>
+
+<BODY>
+
+<TABLE WIDTH="98%" CELLPADDING="0" CELLSPACING="0" BORDER="0">
+<TR>
+   <TD><A HREF="/"><IMG SRC="/images/freshports.jpg" ALT="FreshPorts.org - the place for ports" WIDTH="512" HEIGHT="110" BORDER="0"></A>
+  </TD>
+   <TD ALIGN="right" CLASS="sans" VALIGN="bottom"><small><? echo date("D, j M Y g:i A T") ?></small>
+  </TD>
+</TR>
+</TABLE>
+
+<H1>FreshPorts Development Site - other copyrights</H1>
+
+<TABLE WIDTH="<? echo $TableWidth; ?>" ALIGN="left" BORDER="0">
+  <TR>
+	<TD VALIGN="top" WIDTH="100%">
+	<P>
+	The copyright on the daemon you see in the website logo is as follows:
+	</P>
+
+<BLOCKQUOTE>
+	<P>
+	BSD Daemon Copyright 1988 by Marshall Kirk McKusick.<BR>
+	All Rights Reserved.<BR>
+<BR>
+	Permission to use the daemon may be obtained from:<BR>
+<BLOCKQUOTE>
+		Marshall Kirk McKusick<BR>
+		1614 Oxford St<BR>
+		Berkeley, CA 94709-1608<BR>
+		USA<BR>
+</BLOCKQUOTE>
+	or via email at mckusick&#64;mckusick.com<BR>
+
+</BLOCKQUOTE>
+	</TD>
+  </TR>
+
+</TABLE>
+
+</BODY>
+</HTML>
+
