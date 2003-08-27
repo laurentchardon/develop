65edc22bad7f1049792b92a293cb981145a617db
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-08-27 18:35:33 +0000

    first commit

---
+++ www/archive-processing/index.php
@@ -0,0 +1,30 @@
+<?php
+	$Title = 'FreshPorts - Processing mail archives';
+?>
+<html>
+
+<head>
+<title><?php echo $Title; ?></title>
+</head>
+
+<body>
+
+<h1><?php echo $Title; ?></h1>
+
+<h2>Goals</h2>
+The <a href="goal.php">goals</a> of this project are pretty simple.  We need a way to process mail archives.
+
+<h2>The solutions</h2>
+
+Within hours of putting out a call for help, I had three solutions.  My thanks to those who 
+contributed.
+
+<ul>
+<li><a href="catchup.php">catchUp.pl</a> by Stanislav Grozev
+<li><a href="cvs-arch.php">cvs-arch.pl</a> by Peter Pentchev
+<li><a href="split.php">split-by-messageid</a> by Dirk Meyer
+</ul>
+
+</body>
+
+</html>
