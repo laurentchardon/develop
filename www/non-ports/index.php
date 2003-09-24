2e9079622e45a70133c33233e16cc13cfaea82b0
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2003-09-24 14:29:05 +0000

    Update to latest status

--- www/non-ports/index.php
+++ www/non-ports/index.php
@@ -11,8 +11,9 @@
 
 <h1><?php echo $Title; ?></h1>
 
-<h2>Goals</h2>
 <p align="right"><small>22 September 2003</small></p>
+
+<h2>Goals</h2>
 
 <p>
 The purpose of this project is to allow users to view the commits of previously
@@ -35,6 +36,14 @@ However, changes occur in the ports tree which are not directly related to any p
 port.  For example, the Mk and Tools directories contain files which directly affect how
 ports are built.  Including the commits to these items will increase the usefulness of FreshPorts
 and will expand the scope of coverage to the entire ports tree, not just the ports.
+
+<h2>Current Status</h2>
+
+<p>
+<ul>
+<li>The database handles and includes non-port port tree commits
+<li>date.php needs to be modified
+</ul>
 
 <h2>Changes to FreshPorts</h2>
 
@@ -60,7 +69,15 @@ to the latest_commits_ports table.
 <h3>index.php</h3>
 <p>
 index.php needs to be modified to handle non-port commits.  The SQL has already been modified to cater
-for non-ports.  New classes will be required to display non-port data.
+for non-ports.  New classes will be required to display non-port data.  A first step has been to 
+create a function which returns the latest commits: 
+
+<blockquote><pre class="code">
+FUNCTION LastestCommits(int, int) RETURNS SETOF commit_record 
+</code></blockquote>
+
+<p>
+This function now (24 Sep 2003) supports non-port tree commits.
 
 <h3>date.php</h3>
 <p>
@@ -70,10 +87,49 @@ See index.php.
 We will need new classes which will display the correct information for a commit.  This will be used for
 index.php, date.php, and possibly other locations.
 
+<p>
+Done.  See classes/commit_record.php
+
+<blockquote><pre class="code">
+</code></blockquote>
+
 <h2>Sample database</h2>
 
 <p>
-Not available yet.
+Here is the record type which is returned to index.php:
+
+<blockquote><pre class="code">
+CREATE TYPE commit_record AS (
+	commit_log_id        integer,
+	commit_date_raw      timestamp with time zone,
+	message_subject      text,
+	message_id           text,
+	committer            text,
+	commit_description   text,
+	commit_date          text,
+	commit_time          text,
+	encoding_losses      boolean,
+	port_id              integer,
+	needs_refresh        smallint,
+	forbidden            text,
+	broken               text,
+	element_id           integer,
+	version              text,
+	revision             text,
+	date_added           integer,
+	short_description    text,
+	category_id          integer,
+	port                 text,
+	status               character(1),
+	category             text,
+	security_notice_id   integer,
+	watch                bigint,
+	element_pathname     text
+);
+</code></blockquote>
+
+<p>
+Done.  See database-schema/datatype.txt
 
 <h2>Language</h2>
 
